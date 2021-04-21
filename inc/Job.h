c7d27d43e5f04e988f48548fa2e4762dd522cda3
Austin Chang <achang5@nd.edu>
2021-04-21 17:22:26 -0400

    Job Header File: Job.h

--- inc/Job.h
+++ inc/Job.h
@@ -1,3 +1,22 @@
 #include <iostream>
 
+class Job
+{
+	private:
+		float time;
+		float memory;
 
+	public:
+		float get_time() const;
+		float get_memory() const;
+
+		// Default Constructor
+		Job();
+	
+		// Overloaded Constructor
+		Job(const float &in_time, const float &in_memory);
+
+		float set_time(const float &in_time);
+		float set_memory(const float &in_memory);
+
+};
