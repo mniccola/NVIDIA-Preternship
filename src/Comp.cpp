d34984f02181e9b947a8a545d9b265c75e082e22
Matthew Niccolai <mniccola@nd.edu>
2021-05-05 16:34:55 -0400

    fixed some issues that stopped compilation

--- src/Comp.cpp
+++ src/Comp.cpp
@@ -1,11 +1,11 @@
 #include "../inc/Comp.h"
 
 // Default Constructor
-Comp::Comp() : totalMemory(100) avaliableMemory(100) {}
+Comp::Comp() : totalMemory(100), avaliableMemory(100) {}
 
 // Overloaded Constructor
 Comp::Comp(const float& in_memory, const float& in_available)
-	: totalMemory(in_memory) avaliableMemory(in_available)  {}
+	: totalMemory(in_memory), avaliableMemory(in_available)  {}
 
 // get memory used by computer
 float Comp::get_memory() const {
@@ -18,7 +18,7 @@ float Comp::get_avaliable() const {
 
 // set memory used by computer
 void Comp::set_memory(const float& in_memory){
-	memory = in_memory;
+	totalMemory = in_memory;
 }
 
 void Comp::update_avaliable(const float& in_available){
